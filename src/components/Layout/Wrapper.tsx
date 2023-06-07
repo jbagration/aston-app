443b10d704f5289887cd3dcbe3e470f2e1b2bf4a
Yulia Ivanova <113036475+jbagration@users.noreply.github.com>
2023-06-07 19:21:21 +0500

    fix: delete (June 7, 2023, 19:21)

--- src/components/Layout/Wrapper.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Props } from '../../types/types';
-
-const Wrapper: React.FC<Props> = ({ children }) => (
-  <div className='wrapper'>{children}</div>
-);
-
-export default Wrapper;
