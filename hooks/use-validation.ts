98edf4bdaeeadad57d414208a13ed2951f67176a
Yulia Ivanova <113036475+jbagration@users.noreply.github.com>
2023-06-08 16:03:00 +0500

    fix: Update use-validation.ts (8 June, 16:02)
    ф-ция validateInput вынесена за пределы хука

--- hooks/use-validation.ts
+++ hooks/use-validation.ts
@@ -1,31 +1,31 @@
 import { useState, useRef } from 'react';
 
+const validateInput = (input: string, inputType: string) => {
+  let regex: RegExp;
+
+  switch (inputType) {
+    case 'email':
+      regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      break;
+
+    case 'password':
+      regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{5,}$/;
+      break;
+
+    default:
+      console.log('Input type is not defined');
+      return false;
+  }
+
+  return regex.test(input);
+};
+
 const useValidation = (inputType: string) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [isInvalid, setIsInvalid] = useState(false);
 
-  const validateInput = (input: string) => {
-    let regex: RegExp;
-
-    switch (inputType) {
-      case 'email':
-        regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        break;
-
-      case 'password':
-        regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{5,}$/;
-        break;
-
-      default:
-        console.log('Input type is not defined');
-        return false;
-    }
-
-    return regex.test(input);
-  };
-
   const submitValueHandler = (currentValue: string) => {
-    let isValueValid = validateInput(currentValue);
+    let isValueValid = validateInput(currentValue, inputType);
 
     setIsInvalid(!isValueValid);
 
