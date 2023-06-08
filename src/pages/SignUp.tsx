b1cb8b75f26752d7844710342d84d1ef64cb6622
Yulia Ivanova <113036475+jbagration@users.noreply.github.com>
2023-06-08 17:55:11 +0500

    fix: Update SignUp.tsx (8 June, 2023 17:55)
    навигация перенесена на уровень выше

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,13 +1,18 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import AuthForm from '../components/AuthForm';
 import { RootState } from '../types/types';
 
 const SignUp = () => {
+  const navigate = useNavigate();
   const { email: user } = useSelector<RootState, any>((state) => state.user);
 
-  return user ? <Navigate to='/' replace /> : <AuthForm header='Sign Up' type='signup' />;
+  if (user) {
+    return <Navigate to='/' replace />;
+  }
+
+  return <AuthForm header='Sign Up' type='signup' />;
 };
 
 export default SignUp;
